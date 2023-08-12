6b7dc083f72c0d59eb9baa337ce5fb004e75b3e6
CurlyDolphin <dmitriyh.salnikov@gmail.com>
2023-08-12 12:37:46 +0300

    Testing react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import {Counter} from "./Components/Counter";
-import "./reset.scss"
+import React, { Suspense } from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import './reset.scss';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
-        <div>
-            <Counter></Counter>
+        <div className="app">
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
