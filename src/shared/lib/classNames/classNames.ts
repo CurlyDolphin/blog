cc181f546dd1db1230a766d84afa3da77da33a0b
CurlyDolphin <dmitriyh.salnikov@gmail.com>
2023-08-28 18:44:28 +0300

    Create Navbar and AppLink components

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -7,6 +7,6 @@ export const classNames = (cls: string, mods?: Mods, additional?: string[] ): st
         ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className, value]) => className)
-    ].join('')
+    ].join(' ')
 }
 
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,37 @@
+import React, {ReactNode} from 'react';
+import {Link, LinkProps} from "react-router-dom";
+import cls from "./AppLink.module.scss";
+import {classNames} from "shared/lib/classNames/classNames";
+
+export enum AppLinkTheme {
+    PRIMARY = "primary",
+    SECONDARY = "secondary",
+    RED = "red"
+}
+
+interface AppLinkProps extends LinkProps {
+    className?: string;
+    theme?: AppLinkTheme;
+    children: ReactNode;
+}
+
+export const AppLink = (props: AppLinkProps) => {
+
+    const {
+        to,
+        className,
+        children,
+        theme = AppLinkTheme.PRIMARY,
+        ...otherProps
+    } = props
+
+    return (
+        <Link
+            to={to}
+            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+};
